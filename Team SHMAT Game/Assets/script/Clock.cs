798e1ecb3e61bf419460c486d11256380d84db2c
zizomass <zizomass@outlook.com>
2020-03-04 20:58:54 -0500

    UI model
    Mainscreen and game ui model

---
+++ Team SHMAT Game/Assets/script/Clock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 300f;
+
+    [SerializeField] TextMeshProUGUI timer;
+
+    void Start ()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update ()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timer.text = currentTime.ToString("0");
+    }
+}
