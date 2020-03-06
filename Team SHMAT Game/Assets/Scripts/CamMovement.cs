21aef0e6cfa739deb0006298d32388eb46f7672f
Adam Gill <pocketman3915@gmail.com>
2020-03-06 01:32:50 -0500

    Make bumper glow and dash effect

--- Team SHMAT Game/Assets/Scripts/CamMovement.cs
+++ Team SHMAT Game/Assets/Scripts/CamMovement.cs
@@ -29,7 +29,7 @@ public class CamMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
         if (targets.Count == 0)
         {
